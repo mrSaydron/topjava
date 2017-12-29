c0ca554b8e75dca67d526d0ca5fe55f103fe0419
mrSaydron <mrsaydron@gmail.com>
2017-12-29 17:16:02 +0500

    29.12.2017 HW07_1 complete

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
+import ru.javawebinar.topjava.AuthorizedUser;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,8 +28,20 @@ public class RootControllerTest extends AbstractControllerTest {
                 )));
     }
 
-    /*@Test
+    @Test
     public void testMeals() throws Exception {
-        mockMvc.perform(get(/meals))
-    }*/
+        AuthorizedUser.setId(100001);
+        mockMvc.perform(get("/meals"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("meals"))
+            .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+            .andExpect(model().attribute("meals", hasSize(2)))
+            .andExpect(model().attribute("meals", hasItem(
+                    allOf(
+                            hasProperty("id", is(100008)),
+                            hasProperty("description", is("Админ ланч"))
+                    )
+            )));
+    }
 }
